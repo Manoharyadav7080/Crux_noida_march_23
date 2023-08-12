d0129664b236667146da4b6799c0bb799e669735
Manohar <yadavmanohar78094@email.com>
2023-08-12 12:48:13 +0530

    DataType1

---
+++ Lec5/Data_Type.java
@@ -0,0 +1,13 @@
+package Lec5;
+
+public class Data_Type {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 90;
+		short s = 11;
+		long l = 89;
+
+	}
+
+}
