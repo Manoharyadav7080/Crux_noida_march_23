a48f7f9e68b0635a6e12af45490d44bef59c7491
Manohar <yadavmanohar78094@email.com>
2023-08-15 20:41:48 +0530

    function

---
+++ Lec8/Function_demo.java
@@ -0,0 +1,30 @@
+package Lec8;
+
+
+
+//     without parameter 
+public class Function_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey");
+		System.out.println("bye");
+		System.out.println(add());
+
+	}
+	public static int add() {
+		int a = 9;
+		int b= 10;
+		int c = a+b;
+		System.out.println(sub());
+		return c;
+	}
+	
+	public static int sub() {
+		int a = 9;
+		int b = 10 ;
+		int c = a-b ;
+		return c;
+	}
+
+}
